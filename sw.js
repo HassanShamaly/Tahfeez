be24438935e93c553da36d38e24bcd3aa6cf398f
HassanShamaly <67371443+HassanShamaly@users.noreply.github.com>
2026-07-15 17:07:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* التخزين المؤقت للعمل دون تحميل متكرر (التعرف الصوتي نفسه يتطلب إنترنت) */
-const CACHE = 'tasmee4-v4';
+const CACHE = 'tasmee4-v5';
 const ASSETS = ['./', './index.html', './matcher.js', './quran.json', './manifest.json',
                 './icons/icon-192.png', './icons/icon-512.png'];
 
